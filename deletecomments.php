1a51e8ef9ce00630455fd3d88cc6d77d309a3c2c
njain629 <njain629@gmail.com>
2018-01-19 17:24:56 +0530

    delete comments

---
+++ deletecomments.php
@@ -0,0 +1,21 @@
+<?php
+require_once("include/db.php");
+require_once("include/session.php");
+require_once("include/function.php");
+$con=mysqli_connect('localhost','root','','project');
+if (isset($_GET['id'])) {
+  $idfromurl=$_GET['id'];
+  $query="DELETE FROM comments WHERE id='$idfromurl'";
+  $execute=mysqli_query($con,$query);
+  if ($execute) {
+    $_SESSION['successmessage']="Comment Deleted Successfully";
+      redirect("comments.php");
+  }else{
+    $_SESSION['errormessage']="Something went wrong. Try again";
+    redirect("comments.php");
+  }
+
+}
+
+
+?>
